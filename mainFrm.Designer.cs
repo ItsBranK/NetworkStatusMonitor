893e3cc709281ae7ac08493fed0b58f48f185d3b
BranK <23649425+ItsBranK@users.noreply.github.com>
2020-08-28 23:01:17 -0400

    Initial Commit

---
+++ mainFrm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Internet_Status_Monitor
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.monitorTmr = new System.Windows.Forms.Timer(this.components);
+            this.monitorBtn = new System.Windows.Forms.Button();
+            this.logList = new System.Windows.Forms.ListView();
+            this.typeColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timeColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.durationColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.watchTmr = new System.Windows.Forms.Timer(this.components);
+            this.nicLbl = new System.Windows.Forms.Label();
+            this.nicBox = new System.Windows.Forms.ComboBox();
+            this.checkBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monitorTmr
+            // 
+            this.monitorTmr.Interval = 1000;
+            this.monitorTmr.Tick += new System.EventHandler(this.monitorTmr_Tick);
+            // 
+            // monitorBtn
+            // 
+            this.monitorBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monitorBtn.Location = new System.Drawing.Point(12, 246);
+            this.monitorBtn.Name = "monitorBtn";
+            this.monitorBtn.Size = new System.Drawing.Size(160, 30);
+            this.monitorBtn.TabIndex = 0;
+            this.monitorBtn.Text = "Start Monitoring";
+            this.monitorBtn.UseVisualStyleBackColor = true;
+            this.monitorBtn.Click += new System.EventHandler(this.monitorBtn_Click);
+            // 
+            // logList
+            // 
+            this.logList.BackColor = System.Drawing.Color.White;
+            this.logList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.typeColumn,
+            this.statusColumn,
+            this.timeColumn,
+            this.durationColumn});
+            this.logList.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logList.ForeColor = System.Drawing.Color.Black;
+            this.logList.FullRowSelect = true;
+            this.logList.GridLines = true;
+            this.logList.HideSelection = false;
+            this.logList.Location = new System.Drawing.Point(12, 40);
+            this.logList.MultiSelect = false;
+            this.logList.Name = "logList";
+            this.logList.Size = new System.Drawing.Size(490, 200);
+            this.logList.TabIndex = 18;
+            this.logList.UseCompatibleStateImageBehavior = false;
+            this.logList.View = System.Windows.Forms.View.Details;
+            // 
+            // typeColumn
+            // 
+            this.typeColumn.Text = "Connection Type";
+            this.typeColumn.Width = 120;
+            // 
+            // statusColumn
+            // 
+            this.statusColumn.Text = "Status";
+            this.statusColumn.Width = 136;
+            // 
+            // timeColumn
+            // 
+            this.timeColumn.Text = "Time";
+            this.timeColumn.Width = 121;
+            // 
+            // durationColumn
+            // 
+            this.durationColumn.Text = "Duration";
+            this.durationColumn.Width = 112;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBtn.Location = new System.Drawing.Point(177, 246);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(160, 30);
+            this.saveBtn.TabIndex = 28;
+            this.saveBtn.Text = "Save log";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(342, 246);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(160, 30);
+            this.clearBtn.TabIndex = 29;
+            this.clearBtn.Text = "Clear log";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // watchTmr
+            // 
+            this.watchTmr.Tick += new System.EventHandler(this.watchTmr_Tick);
+            // 
+            // nicLbl
+            // 
+            this.nicLbl.BackColor = System.Drawing.Color.Transparent;
+            this.nicLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nicLbl.ForeColor = System.Drawing.Color.Black;
+            this.nicLbl.Location = new System.Drawing.Point(12, 9);
+            this.nicLbl.Name = "nicLbl";
+            this.nicLbl.Size = new System.Drawing.Size(110, 25);
+            this.nicLbl.TabIndex = 30;
+            this.nicLbl.Text = "Network Interface:";
+            this.nicLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nicBox
+            // 
+            this.nicBox.BackColor = System.Drawing.Color.White;
+            this.nicBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.nicBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nicBox.ForeColor = System.Drawing.Color.Black;
+            this.nicBox.FormattingEnabled = true;
+            this.nicBox.Location = new System.Drawing.Point(128, 10);
+            this.nicBox.Name = "nicBox";
+            this.nicBox.Size = new System.Drawing.Size(293, 23);
+            this.nicBox.TabIndex = 31;
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBtn.Location = new System.Drawing.Point(427, 9);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(75, 25);
+            this.checkBtn.TabIndex = 32;
+            this.checkBtn.Text = "Check";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            this.checkBtn.Click += new System.EventHandler(this.checkBtn_Click);
+            // 
+            // mainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 286);
+            this.Controls.Add(this.checkBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.nicBox);
+            this.Controls.Add(this.nicLbl);
+            this.Controls.Add(this.logList);
+            this.Controls.Add(this.monitorBtn);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "mainFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Internet Status Monitor [Inactive]";
+            this.Load += new System.EventHandler(this.mainFrm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer monitorTmr;
+        private System.Windows.Forms.Button monitorBtn;
+        internal System.Windows.Forms.ListView logList;
+        private System.Windows.Forms.ColumnHeader statusColumn;
+        internal System.Windows.Forms.ColumnHeader timeColumn;
+        internal System.Windows.Forms.ColumnHeader durationColumn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.ColumnHeader typeColumn;
+        private System.Windows.Forms.Timer watchTmr;
+        private System.Windows.Forms.Label nicLbl;
+        private System.Windows.Forms.ComboBox nicBox;
+        private System.Windows.Forms.Button checkBtn;
+    }
+}
+
---
+++ mainFrm.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Internet_Status_Monitor {
+    public partial class mainFrm : Form {
+        private TimeSpan activeTime = new TimeSpan();
+        private TimeSpan durationTime = new TimeSpan();
+        private bool monitoring = false;
+        private bool justStarted = true;
+        private bool activeStatus = false;
+
+        public mainFrm() {
+            InitializeComponent();
+        }
+
+        private void mainFrm_Load(object sender, EventArgs e) {
+            loadNetworks();
+        }
+
+        private void checkBtn_Click(object sender, EventArgs e) {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in nics) {
+                // Finds the selected NIC and compares it's `OperationalStatus`
+                if (adapter.Description == nicBox.Text) {
+                    if (adapter.OperationalStatus == OperationalStatus.Up) {
+                        MessageBox.Show("Information: The selected network interface is online!", "Internet Status Monitor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    } else {
+                        MessageBox.Show("Warning: The selected network interface is not currently online, it may not work properly.", "Internet Status Monitor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+            }
+        }
+
+        // Gets all the available NIC's and adds their `Description` to `nicBox`
+        public void loadNetworks() {
+            nicBox.Items.Clear();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in nics)
+                nicBox.Items.Add(adapter.Description);
+        }
+
+        public void log(bool status) {
+            bool foundNic = false;
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in nics) {
+                // Finds the selected NIC based on its `Description`
+                if (adapter.Description == nicBox.Text) {
+                    int totalItems = logList.Items.Count;
+                    string connectionType = adapter.NetworkInterfaceType.ToString();
+                    string currentStatus = "(null)";
+
+                    if (totalItems > 0) {
+                        if (!justStarted && logList.Items[totalItems - 1].SubItems[3].Text == "Pending") {
+                            logList.Items[totalItems - 1].SubItems[3].Text = string.Format("{0:00}:{1:00}:{2:00}", durationTime.Hours, durationTime.Minutes, durationTime.Seconds);
+                            durationTime = durationTime.Subtract(TimeSpan.FromMilliseconds(durationTime.TotalMilliseconds));
+                        }
+                    }
+
+                    if (status) { currentStatus = "Connected/Online"; }
+                    else { currentStatus = "Disconnected/Offline"; }
+
+                    Regex wirelessEx = new Regex("(Wireless)");
+                    Regex ethernetEx = new Regex("(Ethernet)");
+                    Match wirelessMatch = wirelessEx.Match(connectionType);
+                    Match ethernetMatch = ethernetEx.Match(connectionType);
+
+                    // This just "pretty prints" the connection type, as it `NetworkInterfaceType` doesn't always return a "simple" answer
+                    // If no match is found, it will default to the provided "non pretty" `NetworkInterfaceType`
+                    if (wirelessMatch.Success) {
+                        connectionType = "WiFi/Wireless";
+                    } else if (ethernetMatch.Success) {
+                        connectionType = "Ethernet/Wired";
+                    }
+
+                    // If the interface was lost, we need to manually switch the connection type if it's changed.
+                    // If we don't do this and the user reconnects to a different interface, from lets say ethernet to wifi, it will report the wrong "disconnection type"
+                    if (!justStarted && currentStatus == "Disconnected/Offline") {
+                        if (logList.Items[totalItems - 1].Text != connectionType) {
+                            if (connectionType == "WiFi/Wireless") {
+                                connectionType = "Ethernet/Wired";
+                            } else {
+                                connectionType = "WiFi/Wireless";
+                            }
+                        }
+                    }
+
+                    ListViewItem item = new ListViewItem();
+                    item.Text = connectionType;
+                    item.SubItems.Add(currentStatus);
+                    item.SubItems.Add(DateTime.Now.ToString("MM/dd/yyyy h:mm tt"));
+                    item.SubItems.Add("Pending");
+                    logList.Items.Add(item);
+                    foundNic = true;
+                    // We've already found the selected NIC so theres no need to continue the loop
+                    return;
+                }
+            }
+
+            // If for some reason the NIC was removed or not found, cancel monitoring and refresh the list (most likely the user didn't select a NIC in the first place)
+            if (!foundNic) {
+                monitorBtn.PerformClick();
+                loadNetworks();
+                MessageBox.Show("Error: Could not find the selected network interface, automatically refreshing list.", "Internet Status Monitor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void monitorBtn_Click(object sender, EventArgs e) {
+            if (monitoring) {
+                this.Text = "Internet Status Monitor [Inactive]";
+                monitorBtn.Text = "Start Monitoring";
+                justStarted = false;
+                monitoring = false;
+                // Stops all monitoring timers and re-enables all buttons and boxes
+                monitorTmr.Stop();
+                watchTmr.Stop();
+                nicBox.Enabled = true;
+                checkBtn.Enabled = true;
+                clearBtn.Enabled = true;
+
+                // If the last status was still pending, change its text from "Pending" to "Aborted" plus the last reported `durationTime`
+                int totalItems = logList.Items.Count;
+                if (totalItems > 0) {
+                    if (logList.Items[totalItems - 1].SubItems[3].Text == "Pending")
+                        logList.Items[totalItems - 1].SubItems[3].Text = "[Aborted] " + string.Format("{0:00}:{1:00}:{2:00}", durationTime.Hours, durationTime.Minutes, durationTime.Seconds); ;
+                }
+
+                // Set both `activeTime` and `durationTime` to zero for the next monitor
+                activeTime = activeTime.Subtract(TimeSpan.FromMilliseconds(activeTime.TotalMilliseconds));
+                durationTime = durationTime.Subtract(TimeSpan.FromMilliseconds(durationTime.TotalMilliseconds));
+            } else {
+                this.Text = "Internet Status Monitor [Active: 00:00:00]";
+                monitorBtn.Text = "Stop Monitoring";
+                justStarted = true;
+                monitoring = true;
+                // Starts all monitoring timers and disables the necessary buttons and boxes
+                watchTmr.Start();
+                monitorTmr.Start();
+                nicBox.Enabled = false;
+                checkBtn.Enabled = false;
+                clearBtn.Enabled = false;
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e) {
+            logList.Items.Clear();
+        }
+
+        // Turns all items in `logList` to a string and writes that to a new file on the users desktop
+        private void saveBtn_Click(object sender, EventArgs e) {
+            int totalItems = logList.Items.Count;
+            if (totalItems > 0) {
+                string saveData = "";
+                saveData += "Connection Type, Status, Time, Duration\n";
+
+                for (int i = 0; i < totalItems; i++) {
+                    string connectionType = logList.Items[i].Text;
+                    string status = logList.Items[i].SubItems[1].Text;
+                    string time = logList.Items[i].SubItems[2].Text;
+                    string duration = logList.Items[i].SubItems[3].Text;
+                    saveData += connectionType + ", " + status + ", " + time + ", " + duration + "\n";
+                }
+
+                string fileName = "LOG " + DateTime.Now.ToString("MM/dd/yyyy h:mm tt");
+                fileName = fileName.Replace("/", "");
+                fileName = fileName.Replace(":", "");
+                fileName = fileName.Replace(" ", "_");
+
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+                using (StreamWriter sw = File.CreateText(desktopPath + fileName + ".txt")) {
+                    sw.WriteLine(saveData);
+                }
+
+                MessageBox.Show("Sucessfully saved log to: \"" + desktopPath + fileName + ".txt\"");
+            }
+        }
+
+        // Separate timer from `monitorTmr` to manager both TimeSpan's and upate `timeLbl` 
+        private void watchTmr_Tick(object sender, EventArgs e) {
+            activeTime = activeTime.Add(TimeSpan.FromMilliseconds(100));
+            durationTime = durationTime.Add(TimeSpan.FromMilliseconds(100));
+            this.Text = "Internet Status Monitor [Active: " + string.Format("{0:00}:{1:00}:{2:00}", activeTime.Hours, activeTime.Minutes, activeTime.Seconds) + "]";
+        }
+
+        private void monitorTmr_Tick(object sender, EventArgs e) {
+            if (monitoring) {
+                // Returns if accessing the web is possible, aka if you're connected to the internet or not
+                bool currentStatus = NetworkInterface.GetIsNetworkAvailable();
+                // If the user just pressed "Start Monitoring" log the initial connection type and set the `activeStatus`
+                if (justStarted) {
+                    log(currentStatus);
+                    activeStatus = currentStatus;
+                    justStarted = false;
+                }
+                // If there is a difference between the last updated `activeStatus` and the one just grabbed by the timer tick (`currentStatus`), log that the status has changed
+                if (currentStatus != activeStatus) {
+                    log(currentStatus);
+                    activeStatus = currentStatus;
+                }
+            }
+        }
+    }
+}
